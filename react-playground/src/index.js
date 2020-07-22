448a6189422ab96b595b5d2fb6ceb2172595ee31
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-07-22 13:25:53 -0500

    drill 2

--- react-playground/src/index.js
+++ react-playground/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
-import HelloWorld from './state-drills/HelloWorld'
+import Bomb from './state-drills/bomb';
 
-ReactDOM.render(<HelloWorld />, document.getElementById('root'));
+ReactDOM.render(<Bomb />, document.getElementById('root'));
